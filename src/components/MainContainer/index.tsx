7daac3a6c4b716a11cbc0ef7011b06110b1ed1a8
Herquiloide Hele <herquiloide@gmail.com>
2021-11-29 15:11:27 +0200

    testing pretier config

--- src/components/MainContainer/index.tsx
+++ src/components/MainContainer/index.tsx
@@ -1,52 +1,48 @@
-import React from "react";
-import styled from "styled-components";
-import SideBar from "./SideBar";
-import RightContent from "./RightContent";
-import BreakPoints from '../../utils/Breakpoints'
-import {useFetchForecast} from "../../hooks/useFetch";
+import React from 'react';
+import styled from 'styled-components';
+import SideBar from './SideBar';
+import RightContent from './RightContent';
+import BreakPoints from '../../utils/Breakpoints';
+import { useFetchForecast } from '../../hooks/useFetch';
 const MainContainer: React.FC = () => {
+	const { error } = useFetchForecast(40);
 
-    const {error} = useFetchForecast(40);
+	if (error) return <h3>Erro occured</h3>;
 
-    if (error) return <h3>Erro occured</h3>
-
-    return (
-        <Main>
-            <SideBar/>
-            <RightContent/>
-        </Main>
-    )
-}
-
+	return (
+		<Main>
+			<SideBar />
+			<RightContent />
+		</Main>
+	);
+};
 
 const Main = styled.main`
-  height: 90vh;
-  width: 90vw;
-  background-color: #F7F6F9;
-  border-radius: 30px;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 2em;
-  display: flex;
-  overflow: hidden;
-  max-width: 1500px;
-  max-height: 1500px;
-  
-  
-  @media all and ${BreakPoints.preMedium}{
-    border: 0;
-    flex-direction: column;
-    border-radius: 0;
-  }
+	height: 90vh;
+	width: 90vw;
+	background-color: #f7f6f9;
+	border-radius: 30px;
+	margin-left: auto;
+	margin-right: auto;
+	margin-top: 2em;
+	display: flex;
+	overflow: hidden;
+	max-width: 1500px;
 
-  @media all and ${BreakPoints.medium}{
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    border-radius: 0;
-  }
-  
-  
+	max-height: 1500px;
+
+	@media all and ${BreakPoints.preMedium} {
+		border: 0;
+		flex-direction: column;
+		border-radius: 0;
+	}
+
+	@media all and ${BreakPoints.medium} {
+		width: 100%;
+		height: 100%;
+		margin: 0;
+		border-radius: 0;
+	}
 `;
 
 export default MainContainer;
--- src/store/GlobalStore.tsx
+++ src/store/GlobalStore.tsx
@@ -1,77 +1,68 @@
-import React, {useState} from "react";
-import {UnitiMeasurement, Weather, WeatherForecast} from '../models'
-
+import React, { useState } from 'react';
+import { UnitiMeasurement, Weather, WeatherForecast } from '../models';
 
 //Initializing Instances
-const _unitMeasurement: UnitiMeasurement = {name: 'metric', symbol: '°C'};
-const _weather: Weather = {};
+const _unitMeasurement: UnitiMeasurement = { name: 'metric', symbol: '°C' };
+const _weather: Weather = {} as Weather;
 const _weatherForecast: WeatherForecast[] = [];
-const _queryCity: string = "Maputo";
+const _queryCity: string = 'Maputo';
 const _loading: boolean = true;
 
-
 export const GlobalContext = React.createContext({
-    unitMeasurement: _unitMeasurement,
-    weather: _weather,
-    weatherForecast: _weatherForecast,
-    queryCity: _queryCity,
-    loading: _loading,
-    setUnitMeasurement: (_: UnitiMeasurement) => {},
-    setWeather: (_: Weather) => {},
-    setWeatherForecast: (_: WeatherForecast[]) => {},
-    setQueryCity: (_: string) => {},
-    setLoading: (_: boolean) => {},
+	unitMeasurement: _unitMeasurement,
+	weather: _weather,
+	weatherForecast: _weatherForecast,
+	queryCity: _queryCity,
+	loading: _loading,
+	setUnitMeasurement: (_: UnitiMeasurement) => {},
+	setWeather: (_: Weather) => {},
+	setWeatherForecast: (_: WeatherForecast[]) => {},
+	setQueryCity: (_: string) => {},
+	setLoading: (_: boolean) => {},
 });
 
-export const GlobalStore: React.FC = ({children}) => {
+export const GlobalStore: React.FC = ({ children }) => {
+	//States
+	const [unitMeasurement, _setUnitMeasurement] = useState(_unitMeasurement);
+	const [weather, _setWeather] = useState(_weather);
+	const [weatherForecast, _setWeatherForecast] = useState(_weatherForecast);
+	const [queryCity, _setQueryCity] = useState(_queryCity);
+	const [loading, _setLoading] = useState(_loading);
 
-    //States
-    const [unitMeasurement, _setUnitMeasurement] = useState(_unitMeasurement);
-    const [weather, _setWeather] = useState(_weather);
-    const [weatherForecast, _setWeatherForecast] = useState(_weatherForecast);
-    const [queryCity, _setQueryCity] = useState(_queryCity);
-    const [loading, _setLoading] = useState(_loading);
+	//Setters
+	function setUnitMeasurement(unit: UnitiMeasurement) {
+		_setUnitMeasurement(unit);
+	}
 
+	function setWeather(weather: Weather) {
+		_setWeather(weather);
+	}
 
-    //Setters
-    function setUnitMeasurement(unit: UnitiMeasurement) {
-        _setUnitMeasurement(unit)
-    }
+	function setWeatherForecast(weatherForecast: WeatherForecast[]) {
+		_setWeatherForecast(weatherForecast);
+	}
 
-    function setWeather(weather: Weather){
-        _setWeather(weather)
-    }
+	function setQueryCity(queryCity: string) {
+		_setQueryCity(queryCity);
+	}
 
-    function setWeatherForecast(weatherForecast: WeatherForecast[]){
-        _setWeatherForecast(weatherForecast)
-    }
+	function setLoading(loading: boolean) {
+		_setLoading(loading);
+	}
 
-    function setQueryCity(queryCity: string){
-        _setQueryCity(queryCity)
-    }
+	//wrapping all context together
+	const allContext = {
+		unitMeasurement,
+		weather,
+		weatherForecast,
+		queryCity,
+		loading,
+		setUnitMeasurement,
+		setWeather,
+		setWeatherForecast,
+		setQueryCity,
+		setLoading,
+	};
 
-    function setLoading(loading: boolean){
-        _setLoading(loading)
-    }
-
-    //wrapping all context together
-    const allContext = {
-        unitMeasurement,
-        weather,
-        weatherForecast,
-        queryCity,
-        loading,
-        setUnitMeasurement,
-        setWeather,
-        setWeatherForecast,
-        setQueryCity,
-        setLoading,
-    };
-
-
-    return (
-        <GlobalContext.Provider value={allContext}>
-            {children}
-        </GlobalContext.Provider>
-    )
-}
+	return <GlobalContext.Provider value={allContext}>{children}</GlobalContext.Provider>;
+};
